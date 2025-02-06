10e25eb1348efe709365e9b0880c9ec9a6ba4c84
Zhi Li <lzblack@users.noreply.github.com>
2025-02-06 11:31:47 -0500

    Finish session 05

---
+++ session06/my_polygon.py
@@ -0,0 +1,32 @@
+import turtle
+
+
+leo = turtle.Turtle()
+
+
+# for i in range(4):
+#     leo.fd(100)
+#     leo.lt(90)
+
+"""Exercise 2.1"""
+
+
+def square(t):
+    """
+    
+    """
+    for i in range(4):
+        t.fd(100)
+        t.lt(90)
+
+
+# square(leo)
+
+"""Exercise 2.2"""
+
+
+def square(t, length):
+    """"""
+
+
+turtle.mainloop()
---
+++ session06/review.py
@@ -0,0 +1,42 @@
+# def add(a, b):
+#     """Return the sum of two numbers"""
+#     s = a + b
+#     return s
+
+
+# result = add(1, 2)
+# print(result)
+# print(s)
+
+
+# def f(x):
+#     return x * 2, x / 2
+
+
+# res_1, res_2 = f(20)
+# print(res_2)
+
+
+# a = 10.5
+# print(type(a))
+
+# if isinstance(a, (int, float)):
+#     print('It is an integer (or a floating point number).')
+# else:
+#     print('It is not a number.')
+
+
+# def f():
+#     print("Hi")
+#     return 1
+
+# result = f()
+# print(result)
+
+
+def add(a, b):
+    a = 10
+    b = 20
+    return a + b
+
+print(add(1, 2))
