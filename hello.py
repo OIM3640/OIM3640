2d9847124d63b85e337fca8e3d88ceeb33567807
Zhi Li <lzblack@users.noreply.github.com>
2025-09-03 17:40:49 -0400

    Finish Session 03

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-print('Hello world!')
+import math
+
+
+# print(2 + +2)
+
+
+# print(2 - -2)
+
+
+# print(2, 2)
+
+# print(2 + 02)
+
+
+n = 17
+# 19 + n + round(math.pi) * 2
+print(19 + n + round(math.pi) * 2)
+
+# print(math.sqrt(5))
